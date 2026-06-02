d3e7c8f158ad0b5b1dfc6856567bca140c744760
YANNICK kabunga <yankbg2@gmail.com>
2026-06-02 17:58:29 +0300

    implementation of room_code_generator.dart for generating different code for rooms

--- lib/core/utils/room_code_generator.dart
+++ lib/core/utils/room_code_generator.dart
@@ -1,1 +1,12 @@
 //Generates random 6-character room codes like "ALPHA5"
+import 'dart:math';
+
+class RoomCodeGenerator {
+
+  String generateRoomCode() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    final random = Random.secure();
+    return List.generate(6, (index) => chars[random.nextInt(chars.length)]).join();
+  }
+
+}
