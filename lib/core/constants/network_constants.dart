620946e3122fe5531027fcda6c91158799ec0023
YANNICK kabunga <yankbg2@gmail.com>
2026-06-02 17:42:32 +0300

    implementation of network_constants.dart putting all network number in one place.

--- lib/core/constants/network_constants.dart
+++ lib/core/constants/network_constants.dart
@@ -1,1 +1,10 @@
 // all network numbers in one place
+class NetworkConstants {
+  static const  int udpPort = 5501;
+  static const int httpPort = 3000;
+  static const String broadcastAddress = '255.255.255.255';
+  static const int presentMinutes = 20;
+  static const int lateMinutes = 30;
+  static const int broadcastIntervalPresent = 2;
+  static const int broadcastIntervalLate = 10;
+}
