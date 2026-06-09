949a44fe0be9472ece28eec367f7921edaf72571
YANNICK kabunga <yankbg2@gmail.com>
2026-06-09 14:29:26 +0300

    add session table and attendance records table

--- lib/data/database/tables.dart
+++ lib/data/database/tables.dart
@@ -1,1 +1,28 @@
 //Defines the shape of your local SQLite tables using drift's Dart classes
+import 'package:drift/drift.dart';
+
+class Sessions extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get sessionId => text().unique()();
+  TextColumn get courseCode => text()();
+  TextColumn get courseName => text()();
+  TextColumn get lecturerName => text().nullable()();
+  TextColumn get roomCode => text()();
+  IntColumn get startTime => integer()();  // unix timestamp
+  IntColumn get endTime => integer()();
+  TextColumn get presentCutoff => text()();
+  TextColumn get lateCutoff => text()();
+  TextColumn get status => text()();       // ACTIVE, ENDED
+  BoolColumn get synced => boolean().withDefault(const Constant(false))();
+  IntColumn get createdAt => integer()();
+}
+
+class AttendanceRecords extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get sessionId => text()();
+  TextColumn get studentId => text()();
+  TextColumn get status => text()();       // PRESENT, LATE, ABSENT
+  IntColumn get timestamp => integer()();
+  // TextColumn get deviceFingerprint => text()();
+  BoolColumn get synced => boolean().withDefault(const Constant(false))();
+}
